bc276a559a8dcbe6db946347e2db4cc7be4dcb2e
seonmi <zlatjs2@gmail.com>
2018-09-30 15:12:28 +0900

    Update: sequelize

---
+++ ch7:learn-sequelize/models/comment.js
@@ -0,0 +1,15 @@
+module.export = (sequlize, DataTypes) => {
+  return sequelize.define('comment', {
+    comment: {
+      type: DataTypes.STRING(100),
+      allowNull: false,
+    },
+    created_at: {
+      type: DataTypes.DATE,
+      allowNull: true,
+      defaultValue: sequelize.literal('now()'),
+    },
+  }, {
+    timestamps: false
+  });
+};
